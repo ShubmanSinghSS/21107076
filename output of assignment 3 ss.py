37966cf9e48e5a2dfcc39286d05fa7c0a63bad6b
ShubmanSinghSS <105435022+ShubmanSinghSS@users.noreply.github.com>
2022-05-12 18:29:06 +0530

    Add files via upload

---
+++ output of assignment 3 ss.py
@@ -0,0 +1,55 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: D:/Class Material/Python/assignment 3.py ==============
+Q1
+Enter a number5
+Binary equivalent of 5 is 101
+Q2
+Enter expression:20
+20 = 20
+Q3
+Enter an integer454
+a) 414504
+b) 102831.0
+enter radius50
+c) 31415.926535897932
+Enter angle 1:40
+Enter angle 1:50
+d) 5.067775359744324
+Enter x coordinate of first point:60
+Enter y coordinate of first point:5
+Enter x coordinate of second point:9
+Enter y coordinate of second point:8
+e) -0.058823529411764705
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:10
+Enter number of hydrogen atoms in molecule:5
+Enter number of oxygen atoms in molecule:2
+The molecular weight of the compound is 157.1455
